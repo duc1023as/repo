a5d8bd763e9e0048ac64b6c3c533e78e75253eb0
DUC_DO <duc1023as@gmail.com>
2024-06-25 15:43:40 +0700

    change person.cpp

--- source/person.cpp
+++ source/person.cpp
@@ -6,6 +6,6 @@
 Person::Person(int age, std::string firstName, std::string lastName) : mAge(age), mFirstName(firstName), mLastName(lastName) {}
 
 void Person::display(){
-    std::cout<<"-----First Name:+++++++ "<<mFirstName<<"Last Name:++++++++ "<<mLastName<<" ,Age: "<<mAge<<"\n";
+    std::cout<<"-----First Name:+++++++ "<<mFirstName<<"Last Name:++++++++ "<<mLastName<<" ,Age: "<<mAge--<<"123456\n";
 }
 
